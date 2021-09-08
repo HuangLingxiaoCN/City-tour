136aed250e91f2609a5763fe95e36dd775530888
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-09-08 15:09:02 +0300

    add four different cities and give some styles

--- src/components/City.js
+++ src/components/City.js
@@ -1,12 +1,12 @@
-import newYork from '../assets/newYork.jpg';
+import './City.css';
 
 export const City = (props) => {
   return (
-    <div>
-      <img src={newYork} alt='New York' width='300px' height='200px' />
-      <h2>New York</h2>
-      <p>New York Travel Guide</p>
-      <p>Info</p>
+    <div className="city">
+      <img src={props.city} alt='New York' width='300px' height='200px' />
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <p>{props.info}</p>
     </div>
   )
 }
--- src/components/CityList.js
+++ src/components/CityList.js
@@ -1,12 +1,17 @@
 import { City } from './City';
+import './CityList.css';
+import London from '../assets/London.jpg';
+import NewYork from '../assets/NewYork.jpg';
+import Shanghai from '../assets/Shanghai.jpg';
+import Tokyo from '../assets/Tokyo.jpg';
 
 const CityList = (props) => {
   return(
-    <div>
-      <City />
-      <City />
-      <City />
-      <City />
+    <div className="CityList">
+      <City city={NewYork} name='NewYork' description='New York Travel Guide' info='Info' />
+      <City city={London} name='London' description='Discover London' info='Info' />
+      <City city={Shanghai} name='Shanghai' description='Visit Shanghai' info='Info' />
+      <City city={Tokyo} name='Tokyo' description='Tokyo in a glance' info='Info' />
     </div>
   )
 }
