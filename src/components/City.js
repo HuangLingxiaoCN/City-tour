f3c44973fadabc66bddf757ecdbb2ab52adedbac
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-09-08 15:17:48 +0300

    add some styles

--- src/components/City.js
+++ src/components/City.js
@@ -3,7 +3,7 @@ import './City.css';
 export const City = (props) => {
   return (
     <div className="city">
-      <img src={props.city} alt='New York' width='300px' height='200px' />
+      <img src={props.city} alt='New York'/>
       <h2>{props.name}</h2>
       <p>{props.description}</p>
       <p>{props.info}</p>
