bdd027d761be8272f5677773888638dd1cbb7e3b
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-09-08 17:56:27 +0300

    adding info content and its styling

--- src/components/City.js
+++ src/components/City.js
@@ -1,12 +1,45 @@
-import './City.css';
+import { useState } from "react";
+import "./City.css";
 
 export const City = (props) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const toggleInfo = () => {
+    setShowInfo((prevState) => {
+      return !prevState;
+    })
+  };
+
   return (
     <div className="city">
-      <img src={props.city} alt='New York'/>
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+        integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp"
+        crossorigin="anonymous"
+      />
+      <img src={props.city} alt="New York" />
       <h2>{props.name}</h2>
       <p>{props.description}</p>
-      <p>{props.info}</p>
+      <p>
+        {props.info}
+        <button onClick={toggleInfo}>
+          <i class="fas fa-sort-down"></i>
+        </button>
+      </p>
+      {showInfo && (
+        <p className="info">
+          Vivamus magna justo, lacinia eget consectetur sed, convallis at
+          tellus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at
+          sem. Curabitur non nulla sit amet nisl tempus convallis quis ac
+          lectus. Proin eget tortor risus. Vestibulum ac diam sit amet quam
+          vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget
+          tincidunt nibh pulvinar a. Nulla porttitor accumsan tincidunt. Mauris
+          blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget
+          tortor risus. Mauris blandit aliquet elit, eget tincidunt nibh
+          pulvinar a.
+        </p>
+      )}
     </div>
-  )
-}
+  );
+};
